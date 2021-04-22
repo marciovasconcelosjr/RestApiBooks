bfab2cc018b0ccb9703d99380e3345363138357b
marciovasconcelos10 <m.junior80@yahoo.com.br>
2021-04-22 20:15:17 -0300

    Changes Commit

--- WebApplication1/Data/VO/PersonVO.cs
+++ WebApplication1/Data/VO/PersonVO.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using WebApplication1.Model.Base;
 
 namespace WebApplication1.Data.VO
@@ -6,17 +7,19 @@ namespace WebApplication1.Data.VO
 
     public class PersonVO
     {
+        //[JsonPropertyName("code")]
         public long Id { get; set; }
 
+        //[JsonPropertyName("Name")]
         public string FirstName { get; set; }
 
-        
+        //[JsonPropertyName("Last_Name")]
         public string LastName { get; set; }
 
-        
+        //[JsonIgnore]
         public string Address { get; set; }
 
-       
+        //[JsonPropertyName("Sex")]
         public string Gender { get; set; }
 
     }
